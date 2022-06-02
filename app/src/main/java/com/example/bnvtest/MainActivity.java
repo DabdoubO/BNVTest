013f28823535cd30508b61215b067bc3b8c1d1cd
DabdoubO <dabdoub.osama@gmail.com>
2022-06-02 20:00:48 +0300

    Initial commit

---
+++ app/src/main/java/com/example/bnvtest/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.bnvtest;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView BNV = findViewById(R.id.botNav);
+        BNV.setOnNavigationItemSelectedListener(navListener);
+        BNV.setSelectedItemId(R.id.navHome);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragmintContainer,new fraghome()).commit();
+
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFrag = null;
+
+                    switch (item.getItemId()){
+                        case R.id.navMenu:
+                            selectedFrag = new fragmenu();
+                            break;
+                        case R.id.navCart:
+                            selectedFrag = new fragcart();
+                            break;
+                        case R.id.navHome:
+                            selectedFrag = new fraghome();
+                            break;
+                        case R.id.navNotif:
+                            selectedFrag = new fragnotif();
+                            break;
+                        case R.id.navProfile:
+                            selectedFrag = new fragprofile();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragmintContainer,selectedFrag).commit();
+                    return true;
+                }
+            };
+}
